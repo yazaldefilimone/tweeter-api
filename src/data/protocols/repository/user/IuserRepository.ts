bdcdc845273048fc0566d5bcd867149c47d54685
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-02-28 18:23:29 +0200

    refactor(data): modified params of repository of user

--- src/data/protocols/repository/user/IuserRepository.ts
+++ src/data/protocols/repository/user/IuserRepository.ts
@@ -1,19 +1,15 @@
 import { CreateUserDto } from '@/domain/user/dtos';
 
 export interface IuserRepository{
-  findOneByEmail: ({ email }:IuserRepository.Input) => Promise<IuserRepository.Output>;
-  delete: ({ id }: IuserRepository.Input) => Promise<null>;
+  findOneByEmail: ({ email }:{ email: string }) => Promise<IuserRepository.Output>;
+  delete: ({ id }: { id:string }) => Promise<null>;
   findAll: () => Promise<IuserRepository.Output[]>;
   add: (data:IuserRepository.Output) => Promise<IuserRepository.Output>;
   update: (data:IuserRepository.Output) => Promise<IuserRepository.Output>
-  findOneById: ({ id }: IuserRepository.Input) => Promise<IuserRepository.Output>;
+  findOneById: ({ id }: { id:string }) => Promise<IuserRepository.Output>;
 }
 
 
 export namespace IuserRepository{
   export type Output = CreateUserDto;
-  export type Input = {
-    id:string;
-    email:string;
-  }
 }
