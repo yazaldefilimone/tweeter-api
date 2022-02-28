71a7bb229458cc7a66323004db6cfa19297a94c4
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-02-28 11:39:42 +0200

    feat(domain): create value object of user

---
+++ src/domain/user/object-value/index.ts
@@ -0,0 +1,30 @@
+import { invalidParamError } from '../errors';
+import { Either, left, right } from '@/shared/error-handler/either';
+import { 
+  isValidBio,
+  isValidName,
+  isValidEmail,
+  isValidPassword,
+  isValidBorn } from "@/shared/validators";
+
+
+export class UserObjectValue{
+  Name(name:string): Either<invalidParamError, string>{
+     return isValidName(name) ? right(name) : left(new invalidParamError('name')) 
+  }
+  Email(email:string): Either<invalidParamError, string>{
+     return isValidEmail(email) ? right(email) : left(new invalidParamError('email')) 
+  }
+  Password(password:string): Either<invalidParamError, string>{
+     return isValidPassword(password) ? right(password) : left(new invalidParamError('password')) 
+  }
+   Bio(bio:string): Either<invalidParamError, string>{
+     return isValidBio(bio) ? right(bio) : left(new invalidParamError('biography')) 
+  }
+  Born(born:string): Either<invalidParamError, string>{
+     return isValidBorn(born) ? right(born) : left(new invalidParamError('born data')) 
+  }
+
+}
+
+
