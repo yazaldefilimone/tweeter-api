8db29fe23671f3a649c44b1e0c0d2a04a4ec9e66
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-02-28 21:10:33 +0200

    refactor(data): add return null of find method

--- src/data/protocols/repository/user/IuserRepository.ts
+++ src/data/protocols/repository/user/IuserRepository.ts
@@ -1,12 +1,12 @@
 import { CreateUserDto } from '@/domain/user/dtos';
 
 export interface IuserRepository{
-  findOneByEmail: ({ email }:{ email: string }) => Promise<IuserRepository.Output>;
-  delete: ({ id }: { id:string }) => Promise<null>;
+  findOneByEmail: ({ email }:{ email: string }) => Promise<IuserRepository.Output | null>;
+  delete: ({ id }: { id:string }) => Promise<void>;
   findAll: () => Promise<IuserRepository.Output[]>;
   add: (data:IuserRepository.Output) => Promise<IuserRepository.Output>;
   update: (data:IuserRepository.Output) => Promise<IuserRepository.Output>
-  findOneById: ({ id }: { id:string }) => Promise<IuserRepository.Output>;
+  findOneById: ({ id }: { id:string }) => Promise<IuserRepository.Output | null>;
 }
 
 
