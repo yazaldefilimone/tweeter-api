08760bb319fed9dae291a8b94fca057937538607
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-01 07:25:19 +0200

    feat(database): create config of typeorm for acess of database

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [
+    `${process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'}/infra/postgres/entities/index.{js,ts}`
+  ],
+  migrations:['./src/infra/postgres/migrations/**.ts'],
+  cli: {
+    migrationsDir:'./src/infra/postgres/migrations'
+  }
+}
