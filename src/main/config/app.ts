3dffcf93969bce9c0464fe422876e37398592771
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-01 11:17:30 +0200

    feat(main): add router  of app

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,3 +1,7 @@
+import { userRouter } from '@/main/routes'
+
+
+
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
@@ -7,5 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(helmet())
 app.use(cors());
+
+
+app.use(userRouter);
 
 export { app } 
