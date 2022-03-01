9ca09908fab275a988f9648eea6c0d6337e713fe
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-01 07:26:31 +0200

    style(main): change exportd default for export

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
-const app = express();
+const app: express.Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(helmet())
 app.use(cors());
 
-export default app;
+export { app } 
