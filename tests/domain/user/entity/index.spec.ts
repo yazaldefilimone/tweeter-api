c626bc094e40d226d254f527357fdab3364d1134
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-02 11:57:12 +0200

    test(infra): ensure return invalidParamsError if not receive correct username

--- tests/domain/user/entity/index.spec.ts
+++ tests/domain/user/entity/index.spec.ts
@@ -40,5 +40,20 @@ describe('User', () => {
     expect(response.isLeft()).toBe(true)
     expect(response.value).toEqual(new invalidParamError('name'))
   })
+  
+  it('should return invalidParamsError if not receive correct username', () => {
+    const { sut } = makeSut()    
+    const user = {
+      name:"any name",
+      username:"invalid_username",
+      born:"21/02/2024",
+      bio:"any biography",
+      email:"email@gmail.com",
+      password:"1234yaza"
+    }
+    const response = sut.build(user);
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toEqual(new invalidParamError('username'))
+  })
 })
 
