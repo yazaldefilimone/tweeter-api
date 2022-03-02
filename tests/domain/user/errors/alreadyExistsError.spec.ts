ac6f0d8a8f3dd43a5e78bb532021c2e0e40be24c
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-02 12:50:27 +0200

    test(domain): ensure return message error of call alreadyExistsError

---
+++ tests/domain/user/errors/alreadyExistsError.spec.ts
@@ -0,0 +1,10 @@
+import { alreadyExistsError } from '../../../../src/domain/user/errors'
+
+
+describe('alreadyExistsError', () => {
+  it('Should return a message of user already exists', () => {
+    const sut = new alreadyExistsError();
+    expect(sut.message).toBe('user already exists.')
+    expect(sut.name).toBe('alreadyExistsError')
+  })
+})
