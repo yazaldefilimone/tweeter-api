5965e0e5f9683d19c73c3fa0c2c6194df93b8079
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-02 11:50:49 +0200

    test(infra): ensure return invalidParamsError if not receive correct emial

---
+++ tests/domain/user/entity/index.spec.ts
@@ -0,0 +1,31 @@
+import { User } from '../../../../src/domain/user/entity';
+import { invalidParamError } from '../../../../src/domain/user/errors'
+
+
+const makeSut = () => {
+
+  const sut = new User()
+  return {
+    sut
+  }
+}
+
+describe('User', () => {
+  it('should return invalidParams if not receive correct emial', async () => {
+    const { sut } = makeSut();
+    
+    const user = {
+      name:"any name",
+      username:"@anyUsername",
+      born:"21/02/2024",
+      bio:"any biography",
+      email:"invaid_email",
+      password:"1234yaza"
+    }
+
+    const response = await sut.build(user);
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toEqual(new invalidParamError('email'))
+  })
+})
