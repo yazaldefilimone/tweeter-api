fbe9b48b535e5c203f2040c420af3299eb36aa40
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-02 07:36:10 +0200

    fix(main): change commit function for connect function

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,10 +4,15 @@ import { env } from '@/shared/env';
 
 import 'reflect-metadata'
 
-PgConnection.getInstance().commit().then(async () => {
+/*PgConnection.getInstance().connect().then(async () => {
   const { app } = await import('@/main/config/app');
   app.listen(Number(env.api_port), () => console.log(`api start at: http://localhost:${env.api_port}`))
 }).catch(console.error)
+*/
 
-
-
+PgConnection.getInstance().connect()
+  .then(async () => {
+    const { app } = await import('@/main/config/app')
+    app.listen(Number(env.api_port), () => console.log(`api start at: http://localhost:${env.api_port}`))
+  })
+  .catch(console.error)
