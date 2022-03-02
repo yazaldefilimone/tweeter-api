16eb3663d4968288fd901c4a027c80ec57222c9c
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-02 12:02:16 +0200

    test(infra): refactor of object user for be create in makeSut

--- tests/domain/user/entity/index.spec.ts
+++ tests/domain/user/entity/index.spec.ts
@@ -3,57 +3,45 @@ import { invalidParamError } from '../../../../src/domain/user/errors'
 
 
 const makeSut = () => {
-
+    const RequestUser = {
+      name:"any name",
+      username:"@anyUsername",
+      born:"21/02/2024",
+      bio:"any biography",
+      email:"email@gmail.com",
+      password:"1234yaza"
+    }
   const sut = new User()
   return {
-    sut
+    sut,
+    user:RequestUser
   }
 }
 
 describe('User', () => {
   it('should return invalidParamsError if not receive correct emial', () => {
-    const { sut } = makeSut()    
-    const user = {
-      name:"any name",
-      username:"@anyUsername",
-      born:"21/02/2024",
-      bio:"any biography",
-      email:"invaid_email",
-      password:"1234yaza"
-    }
+    const { sut, user } = makeSut()    
+    user.email = 'invalid_email'
     const response = sut.build(user);
     expect(response.isLeft()).toBe(true)
     expect(response.value).toEqual(new invalidParamError('email'))
   })
   
   it('should return invalidParamsError if not receive correct name', () => {
-    const { sut } = makeSut()    
-    const user = {
-      name:"a",
-      username:"@anyUsername",
-      born:"21/02/2024",
-      bio:"any biography",
-      email:"email@gmail.com",
-      password:"1234yaza"
-    }
+    const { sut, user } = makeSut()
+    user.name = 'a';
     const response = sut.build(user);
     expect(response.isLeft()).toBe(true)
     expect(response.value).toEqual(new invalidParamError('name'))
   })
   
   it('should return invalidParamsError if not receive correct username', () => {
-    const { sut } = makeSut()    
-    const user = {
-      name:"any name",
-      username:"invalid_username",
-      born:"21/02/2024",
-      bio:"any biography",
-      email:"email@gmail.com",
-      password:"1234yaza"
-    }
+    const { sut, user } = makeSut()
+    user.username = 'invalid_username';
     const response = sut.build(user);
     expect(response.isLeft()).toBe(true)
     expect(response.value).toEqual(new invalidParamError('username'))
   })
+  
 })
 
