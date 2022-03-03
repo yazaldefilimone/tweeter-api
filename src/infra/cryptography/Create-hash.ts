57aca67b9d2df4da6ba0ac5e5c4e018cbc4d8247
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-03 10:50:02 +0200

    test(infra): change bcript for dependece injector

--- src/infra/cryptography/Create-hash.ts
+++ src/infra/cryptography/Create-hash.ts
@@ -2,8 +2,10 @@ import { IcreateHash } from '@/data/protocols/cryptography';
 import bcrypt from 'bcryptjs'
 
 export class CreateHash implements IcreateHash{
+  public encrypt;
+  constructor() { this.encrypt = bcrypt }
   async create(data: IcreateHash.Input, salt = 10): Promise<string>{
-    const passwordhash = await bcrypt.hash(data.password, salt);
+    const passwordhash = await this.encrypt.hash(data.password, salt);
     return passwordhash;
   }
 }
