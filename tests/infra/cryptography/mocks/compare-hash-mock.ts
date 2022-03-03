ea316a4eb34b5560c4c0e4d0ce3b1dc6abd9115a
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-03 11:38:38 +0200

    test(infra): rename of mocks method

--- tests/infra/cryptography/mocks/compare-hash-mock.ts
+++ tests/infra/cryptography/mocks/compare-hash-mock.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs'
 import { IcompareHash } from '@/data/protocols/cryptography'
 
-export class CompareHashSpy implements IcompareHash{
+export class CompareHashMock implements IcompareHash{
   count = 0;
   async compare(data: IcompareHash.Input): Promise<boolean>{
     this.count++
--- tests/infra/cryptography/mocks/create-hash-mock.ts
+++ tests/infra/cryptography/mocks/create-hash-mock.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs'
 import { IcreateHash } from '@/data/protocols/cryptography'
 
-export class CreateHashSpy implements IcreateHash{
+export class CreateHashMock implements IcreateHash{
   count = 0
  async create(data: IcreateHash.Input, salt=10): Promise<string>{
    this.count++
