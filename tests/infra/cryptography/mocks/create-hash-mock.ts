54df258436bfb6a923dd4b269a1ad28a86fb108c
Yazalde Filimome <yazaldefilimon@gmail.com>
2022-03-03 10:38:10 +0200

    test(infra): create mock of createHash

---
+++ tests/infra/cryptography/mocks/create-hash-mock.ts
@@ -0,0 +1,11 @@
+import bcrypt from 'bcryptjs'
+import { IcreateHash } from '@/data/protocols/cryptography'
+
+export class CreateHashSpy implements IcreateHash{
+  count = 0
+ async create(data: IcreateHash.Input, salt=10): Promise<string>{
+   this.count++
+   const hash = await  bcrypt.hash(data.password, salt);
+   return hash
+  }
+}
