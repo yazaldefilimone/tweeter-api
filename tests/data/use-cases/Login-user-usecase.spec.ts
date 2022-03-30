51cebca6d31386af81911963f8319a8a5f4fcbc0
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-03-30 12:21:16 +0200

    test: ensure return invalid password if not receive correct password

--- tests/data/use-cases/Login-user-usecase.spec.ts
+++ tests/data/use-cases/Login-user-usecase.spec.ts
@@ -1,22 +1,31 @@
-import { LoginUserUseCase } from "@/data/use-cases/user"
+import { LoginUserUseCase, SigInUserUseCase } from "@/data/use-cases/user"
 import { invalidParamError, notFoundError } from "@/domain/user/errors";
-import { CompareHashMock } from "@/tests/infra/cryptography/mocks"
+import { CompareHashMock, CreateHashMock } from "@/tests/infra/cryptography/mocks"
 import { InMemoryUserRepository } from "@/tests/infra/repository/user";
 
 
 const MakeSut = () => {
-  const fakeUser = {
+  const fakeUser: any= {
     email:"any@gmail.com",
-    password:'123456578'
+    password:'123456578',
+    name:"any name",
+    username:"@anyUsername",
+    born:"21/02/2024",
+    bio:"any biography",
   }
-  const comparehash = new CompareHashMock();
+
+
   const userRepository = new InMemoryUserRepository()
+  const comparehash = new CompareHashMock();;
+  const createHash = new CreateHashMock();
+  const SigInSut = new SigInUserUseCase(createHash, userRepository)
   const sut = new LoginUserUseCase(userRepository, comparehash);
   return {
     sut,
     comparehash,
     userRepository,
-    fakeUser
+    fakeUser,
+    SigInSut
   }
 }
 
@@ -28,5 +37,15 @@ describe('LoginUserUsecase', () => {
     expect(userOrError.isLeft()).toBe(true);
     expect(userOrError.value).toEqual(new notFoundError("user"))
   })
+  it('should return invalid password if not receive correct password', async () => {
+    const { sut, fakeUser, SigInSut } = MakeSut();
+    await SigInSut.preform(fakeUser);
+    fakeUser.password = "invalidPassword";
+    const userOrError = await sut.preform(fakeUser);
+
+    expect(userOrError.isLeft()).toBe(true);
+    expect(userOrError.value).toEqual(new invalidParamError("password"))
+  })
+
   
 })
