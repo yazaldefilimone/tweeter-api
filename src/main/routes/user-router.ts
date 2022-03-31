43856e80a32ce08ef8b56db73b69e93e20ccd8b3
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-03-31 10:51:52 +0200

    feat:create router of login user

--- src/main/routes/user-router.ts
+++ src/main/routes/user-router.ts
@@ -1,15 +1,15 @@
-import { sigInUserControllerFactore } from '@/main/factories/presentation/controllers/user'
-import { RouterAdapter } from '@/main/adapters'
+import {
+  sigInUserControllerFactore,
+  loginUserControllerFactore,
+} from "@/main/factories/presentation/controllers/user";
 
+import { routerAdapter } from "@/main/adapters";
 
-import { Router } from 'express';
+import { Router } from "express";
 
 const userRouter = Router();
 
+userRouter.post("/", routerAdapter(sigInUserControllerFactore()));
+userRouter.post("/", routerAdapter(loginUserControllerFactore()));
 
-userRouter.post('/', RouterAdapter(sigInUserControllerFactore()));
-
-
-
-
-export { userRouter }
+export { userRouter };
