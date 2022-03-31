e24cfed7f361108ab4bf8feeaeb4a6f86f4ed677
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-03-31 10:49:25 +0200

    fix:change name of adapter

--- src/main/adapters/express-router-adapter.ts
+++ src/main/adapters/express-router-adapter.ts
@@ -1,18 +1,20 @@
-import { Icontroller } from '@/presentation/protocols';
+import { Icontroller } from "@/presentation/protocols";
 
-import { RequestHandler } from 'express';
+import { RequestHandler } from "express";
 
+type Adapter = (Controller: Icontroller) => RequestHandler;
 
-type Adapter = (Controller:Icontroller) => RequestHandler;
+export const routerAdapter: Adapter =
+  (Controller) => async (request, response) => {
+    const Input: Icontroller.Input = {
+      params: request.params,
+      body: request.body,
+    };
+    const { statusCode, body } = await Controller.perform(Input);
 
-export const RouterAdapter: Adapter = Controller => async (request, response) => {
-  const Input:Icontroller.Input = {
-    params: request.params,
-    body:request.body
-  }
-  const  { statusCode, body } = await Controller.perform(Input);
+    const json = [200, 204].includes(statusCode)
+      ? body
+      : { error: body.message };
 
-  const json = [200, 204].includes(statusCode) ? body :  { error: body.message }
-
-  response.status(statusCode).json(json);
-}
+    response.status(statusCode).json(json);
+  };
