56b635972319dd6b03a1f456762da4a7bd4ad6d7
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-03-31 21:38:13 +0200

    fix:update

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,18 +1,20 @@
-import './config/module-alias'
-import { PgConnection } from '@/infra/postgres/config';
-import { env } from '@/shared/env';
+import "./config/module-alias";
+import { PgConnection } from "@/infra/postgres/config";
+import { env } from "@/shared/env";
 
-import 'reflect-metadata'
-
+import "reflect-metadata";
 /*PgConnection.getInstance().connect().then(async () => {
   const { app } = await import('@/main/config/app');
   app.listen(Number(env.api_port), () => console.log(`api start at: http://localhost:${env.api_port}`))
 }).catch(console.error)
 */
 
-PgConnection.getInstance().connect()
+PgConnection.getInstance()
+  .connect()
   .then(async () => {
-    const { app } = await import('@/main/config/app')
-    app.listen(Number(env.api_port), () => console.log(`api start at: http://localhost:${env.api_port}`))
+    const { app } = await import("@/main/config/app");
+    app.listen(Number(env.api_port), () =>
+      console.log(`api start at: http://localhost:${env.api_port}`)
+    );
   })
-  .catch(console.error)
+  .catch(console.error);
