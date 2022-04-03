736e1c1c32013638def130f5db892bc7b2af0250
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-03 14:31:31 +0200

    feat: add create token

--- src/data/use-cases/user/Login-user-usecase.ts
+++ src/data/use-cases/user/Login-user-usecase.ts
@@ -1,30 +1,39 @@
-import { IloginUserUseCase } from "@/domain/user/use-cases/Ilogin-user-usecase";
-import { IuserRepository } from "@/data/protocols/repository/user";
-import { IcompareHash } from "@/data/protocols/cryptography";
-import { invalidParamError, notFoundError } from "@/domain/user/errors";
+import { IloginUserUseCase } from '@/domain/user/use-cases/Ilogin-user-usecase';
+import { IuserRepository } from '@/data/protocols/repository/user';
+import { IcompareHash } from '@/data/protocols/cryptography';
+import { invalidParamError, notFoundError } from '@/domain/user/errors';
 
+import { right, left } from '@/shared/error-handler/either';
+import { createJWT } from '@/shared/webtoken';
 
-import { right, left } from "@/shared/error-handler/either";
-
-export class LoginUserUseCase implements IloginUserUseCase{
-  constructor(
-    private readonly userRepository:IuserRepository,
-    private readonly compatreHash:IcompareHash
-    ){}
-  async preform({ password, email }: IloginUserUseCase.Input): IloginUserUseCase.Output{
-
+export class LoginUserUseCase implements IloginUserUseCase {
+  constructor(private readonly userRepository: IuserRepository, private readonly compatreHash: IcompareHash) {}
+  async preform({ password, email }: IloginUserUseCase.Input): IloginUserUseCase.Output {
     const user = await this.userRepository.findOneByEmail({ email });
 
-    if(!user){
-      return  left(new notFoundError("user"))
+    if (!user) {
+      return left(new notFoundError('user'));
     }
 
-    const isValid = await this.compatreHash.compare({ password, passwordhash:user.password })
+    const isValid = await this.compatreHash.compare({ password, passwordhash: user.password });
 
-
-    if(!isValid){
-      return left(new invalidParamError("password"));
+    if (!isValid) {
+      return left(new invalidParamError('password'));
     }
-    return right(user);
+    const token = createJWT({ id: user.id });
+
+    const userauth = {
+      user: {
+        name: user.name,
+        email: user.email,
+        born: user.born,
+        bio: user.bio,
+        username: user.username,
+        created_at: user.created_at,
+      },
+      token,
+    };
+
+    return right(userauth);
   }
 }
