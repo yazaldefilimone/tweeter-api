6a7ab72f2187ce7e96fd9eb932995c54c60bf67a
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-03 17:25:41 +0200

    feat: create Post entity

---
+++ src/domain/post/entity/Post.ts
@@ -0,0 +1,52 @@
+import { Either, left, right } from '@/shared/error-handler/either';
+import { PostDTO } from '../dtos';
+import { PostObjectValue } from '../object-value';
+import { PostResponse } from './protocols';
+
+type CreateObject = {
+  [key: string]: Either<Error, string>;
+};
+
+export class Post {
+  private readonly postValueObject: PostObjectValue;
+  private constructor() {
+    this.postValueObject = new PostObjectValue();
+  }
+
+  public build(post: PostDTO): PostResponse {
+    const ValuepostOrError: CreateObject = {
+      name: this.postValueObject.Name(post.user.name),
+      username: this.postValueObject.UserName(post.user.username),
+      message: this.postValueObject.PostContent(post.message),
+      // refs: this.postValueObject.Refs(post.message),
+      photo: this.postValueObject.Photo(post.photo),
+    };
+
+    const { name, username, message, photo } = ValuepostOrError;
+
+    if (name.isLeft()) return left(name.value);
+
+    if (username.isLeft()) return left(username.value);
+
+    if (message.isLeft()) return left(message.value);
+
+    if (photo.isLeft()) return left(photo.value);
+
+    const currentdate = new Date().toISOString();
+
+    return right({
+      user: {
+        name: name.value,
+        username: username.value,
+        created_at: currentdate,
+      },
+      message: message.value,
+      photo: photo.value,
+      // ref: [
+      // {
+      //   username: '@joeDio',
+      // },
+      // ],
+    });
+  }
+}
