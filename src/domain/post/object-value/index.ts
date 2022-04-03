3f7794387d9a1545780903ea250f251b85bc382c
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-03 16:20:40 +0200

    fix: auto import files

--- src/domain/post/object-value/index.ts
+++ src/domain/post/object-value/index.ts
@@ -1,24 +1,15 @@
 import { invalidParamError } from '@/domain/errors';
 import { Either, left, right } from '@/shared/error-handler/either';
-import { isValidBio, isValidName, isValidEmail, isValidPassword, isValidBorn, isValidUsername } from '@/shared/validators';
+import { isValidName, isValidUsername } from '@/shared/validators';
 
-export class UserObjectValue {
+export class PostObjectValue {
   Name(name: string): Either<invalidParamError, string> {
     return isValidName(name) ? right(name) : left(new invalidParamError('name'));
   }
-  Email(email: string): Either<invalidParamError, string> {
-    return isValidEmail(email) ? right(email) : left(new invalidParamError('email'));
-  }
-  Password(password: string): Either<invalidParamError, string> {
-    return isValidPassword(password) ? right(password) : left(new invalidParamError('password'));
-  }
-  Bio(bio: string): Either<invalidParamError, string> {
-    return isValidBio(bio) ? right(bio) : left(new invalidParamError('biography'));
-  }
-  Born(born: string): Either<invalidParamError, string> {
-    return isValidBorn(born) ? right(born) : left(new invalidParamError('born data'));
-  }
   UserName(username: string): Either<invalidParamError, string> {
     return isValidUsername(username) ? right(username) : left(new invalidParamError('username'));
+  }
+  PostContent(post: string): Either<invalidParamError, string> {
+    return isValidUsername(post) ? right(post) : left(new invalidParamError('post content'));
   }
 }
--- src/domain/user/use-cases/Ilogin-user-usecase.ts
+++ src/domain/user/use-cases/Ilogin-user-usecase.ts
@@ -1,6 +1,6 @@
 import { Either } from '@/shared/error-handler/either';
 import { CreateUserDto, UserLogin } from '../dtos';
-import { invalidParamError, notFoundError } from '@domain/errors';
+import { invalidParamError, notFoundError } from '@/domain/errors';
 
 export interface IloginUserUseCase {
   preform: (Input: IloginUserUseCase.Input) => IloginUserUseCase.Output;
