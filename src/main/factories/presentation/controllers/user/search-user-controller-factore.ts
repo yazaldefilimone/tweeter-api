c0d57281731de6b04d11f5ad0b2f51e5fec804ba
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-03 15:28:03 +0200

    fix:rename of saerch user controoller

--- src/main/factories/presentation/controllers/user/search-user-controller-factore.ts
+++ src/main/factories/presentation/controllers/user/search-user-controller-factore.ts
@@ -1,7 +1,7 @@
 import { SearchUserController } from '@/presentation/controllers/user';
 import { searchUserUseCaseFactore } from '@/main/factories/data/use-cases/user';
 
-export const SearchUserControllerFactore = () => {
+export const searchUserControllerFactore = () => {
   const searchUserUseCase = searchUserUseCaseFactore();
   const searchUserController = new SearchUserController(searchUserUseCase);
   return searchUserController;
