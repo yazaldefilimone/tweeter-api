741abce2f866e8b3faf49b9d31a7716f7ccbddcb
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-03 17:23:47 +0200

    feat: create photo validate

--- src/domain/post/object-value/index.ts
+++ src/domain/post/object-value/index.ts
@@ -1,7 +1,10 @@
 import { invalidParamError } from '@/domain/errors';
 import { Either, left, right } from '@/shared/error-handler/either';
-import { isValidName, isValidUsername } from '@/shared/validators';
+import { isValidName, isValidPostContent, isValidUsername, isValidPhoto } from '@/shared/validators';
 
+type Ref = {
+  username: string;
+};
 export class PostObjectValue {
   Name(name: string): Either<invalidParamError, string> {
     return isValidName(name) ? right(name) : left(new invalidParamError('name'));
@@ -9,7 +12,21 @@ export class PostObjectValue {
   UserName(username: string): Either<invalidParamError, string> {
     return isValidUsername(username) ? right(username) : left(new invalidParamError('username'));
   }
-  PostContent(post: string): Either<invalidParamError, string> {
-    return isValidUsername(post) ? right(post) : left(new invalidParamError('post content'));
+  PostContent(post?: string): Either<invalidParamError, string> {
+    return isValidPostContent(post) ? right(post as string) : left(new invalidParamError('post content'));
+  }
+
+  // Refs(refs?: Ref[]): Either<invalidParamError, Ref[]> {
+  //   const result = refs?.map((ref) => {
+  //     if (!isValidUsername(ref.username)) {
+  //       return false
+  //     }
+  //     return true
+  //   });
+
+  //   return right(result);
+  // }
+  Photo(photo?: string): Either<invalidParamError, string> {
+    return isValidPhoto(photo) ? right(photo as string) : left(new invalidParamError('photo content'));
   }
 }
