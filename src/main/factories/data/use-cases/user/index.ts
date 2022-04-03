18a0478fef4ec3df8997c7260a9f79be5e8903d3
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-03 15:12:49 +0200

    fix: export files of searchUser

--- src/main/factories/data/use-cases/user/index.ts
+++ src/main/factories/data/use-cases/user/index.ts
@@ -1,2 +1,3 @@
-export * from "./sigIn-user-usecase-factore";
-export * from "./login-user-usecase-factore";
+export * from './sigIn-user-usecase-factore';
+export * from './login-user-usecase-factore';
+export * from './search-user-usecase-factore';
