9d5214ce55afe88fa236c8d1446188dce69a7423
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-03 13:33:46 +0200

    style: remove grap line

--- src/data/protocols/repository/user/IuserRepository.ts
+++ src/data/protocols/repository/user/IuserRepository.ts
@@ -1,15 +1,16 @@
-import { CreateUserDto } from '@/domain/user/dtos';
+import { CreateUserDto } from "@/domain/user/dtos";
 
-export interface IuserRepository{
-  findOneByEmail: ({ email }:{ email: string }) => Promise<IuserRepository.Output | null>;
-  delete: ({ id }: { id:string }) => Promise<void>;
+export interface IuserRepository {
+  findOneByEmail: ({ email, }: { email: string }) => Promise<IuserRepository.Output | null>;
+  delete: ({ id }: { id: string }) => Promise<void>;
   findAll: () => Promise<IuserRepository.Output[]>;
-  add: (data:IuserRepository.Output) => Promise<IuserRepository.Output>;
-  update: (data:IuserRepository.Output) => Promise<IuserRepository.Output>
-  findOneById: ({ id }: { id:string }) => Promise<IuserRepository.Output | null>;
+  add: (data: IuserRepository.Output) => Promise<IuserRepository.Output>;
+  update: (data: IuserRepository.Output) => Promise<IuserRepository.Output>;
+  findOneById: ({ id }: { id: string }) => Promise<IuserRepository.Output | null>;
+  findByUserName: ({ username }: { username: string }) => Promise<IuserRepository.Output | null>;
+  findByName: ({ name }: { name: string }) => Promise<IuserRepository.Output | null>;
 }
 
-
-export namespace IuserRepository{
+export namespace IuserRepository {
   export type Output = CreateUserDto;
 }
