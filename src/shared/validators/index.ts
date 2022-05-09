eb7c648e3172bde22b184c5341752263bc9a57a7
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-09 17:14:23 +0200

    feat: create validatores methods

---
+++ src/shared/validators/index.ts
@@ -0,0 +1,65 @@
+import Joi from 'joi';
+
+const emailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// name may contain spaces
+const nameRegex = /^[a-zA-Z0-9 ]+$/;
+// password not contain space
+const passwordRegex = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,30}$/;
+// github official username validator
+const githubUsernameRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+
+const emailSchema = Joi.object({
+  email: Joi.string().email().required().regex(new RegExp(emailRegex)),
+});
+const nameSchema = Joi.object({
+  name: Joi.string().required().regex(new RegExp(nameRegex)).min(2).max(30),
+});
+const passwordSchema = Joi.object({
+  password: Joi.string().required().regex(new RegExp(passwordRegex)).min(8).max(30),
+});
+const bioSchema = Joi.object({
+  bio: Joi.string().required().min(0).max(200),
+});
+const githubUsernameSchema = Joi.object({
+  githubUsername: Joi.string().required().regex(new RegExp(githubUsernameRegex)).min(1).max(38),
+});
+const postContentSchema = Joi.object({
+  content: Joi.string().required().min(1).max(500),
+});
+
+export function isValidEmail(email: string): boolean {
+  if (!email) return false;
+  const result = emailSchema.validate({ email });
+  return result.error ? false : true;
+}
+
+export function isValidName(name: string): boolean {
+  if (!name) return false;
+  const result = nameSchema.validate({ name });
+  return result.error ? false : true;
+}
+
+export function isValidPassword(password: string): boolean {
+  if (!password) return false;
+  const result = passwordSchema.validate({ password });
+  return result.error ? false : true;
+}
+
+export function isValidBio(bio: string): boolean {
+  if (!bio) return false;
+  const result = bioSchema.validate({ bio });
+  return result.error ? false : true;
+}
+
+export function isValidGithubUsername(githubUsername: string): boolean {
+  if (!githubUsername) return false;
+  const result = githubUsernameSchema.validate({ githubUsername });
+  return result.error ? false : true;
+}
+
+export function isValidPostContent(content: string): boolean {
+  if (!content) return false;
+  const result = postContentSchema.validate({ content });
+  return result.error ? false : true;
+}
