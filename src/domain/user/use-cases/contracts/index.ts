768ff21830cc3eabf61510b1387e6bd1e8affdfd
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-10 11:22:11 +0200

    feat: using userToken

--- src/domain/user/use-cases/contracts/index.ts
+++ src/domain/user/use-cases/contracts/index.ts
@@ -1,7 +1,7 @@
 import { Either } from '@/shared/error-handler/either';
-import { userResponse, userStoreDTO } from '@/domain/user/dtos';
+import { userResponse, userToken } from '@/domain/user/dtos';
 import { InvalidBioError, InvalidEmailError, InvalidNameError, InvalidPasswordError } from '@/domain/user/errors';
-import { AlreadyExistsError } from '@/domain/errors';
+import { AlreadyExistsError, NotFoundError } from '@/domain/errors';
 
 export type felidSignUserContracts =
   | InvalidBioError
@@ -9,4 +9,9 @@ export type felidSignUserContracts =
   | InvalidNameError
   | InvalidPasswordError
   | AlreadyExistsError;
+
+export type felidLoginUserContracts = InvalidEmailError | InvalidPasswordError | NotFoundError;
+
 export type signUserContracts = Either<felidSignUserContracts, userResponse>;
+
+export type loginUserContracts = Either<felidLoginUserContracts, userToken>;
