1f1839e2e62b0df6804bf73de1a12d9b86820374
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-10 10:36:46 +0200

    feat: create entity user

--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -1,34 +1,36 @@
-import { InvalidParamError } from '@/domain/errors/InvalidParamError';
 import { Either, right, left } from '@/shared/error-handler/either';
 import { isValidEmail, isValidName, isValidPassword, isValidBio } from '@/shared/validators';
+import { user } from '@/domain/user/dtos';
+import { buildUserContracts } from './contracts';
+import { InvalidBioError, InvalidEmailError, InvalidNameError, InvalidPasswordError } from './errors';
+
 import { randomUUID } from 'crypto';
-import { user, userStoreDTO } from '../dtos';
 
 type ObjectsBuildType = {
   [key: string]: Either<Error, string>;
 };
 export class User {
-  public isValidEmail(email: string): Either<InvalidParamError, string> {
+  public isValidEmail(email: string): Either<InvalidEmailError, string> {
     const isValid = isValidEmail(email);
-    return isValid ? right(email) : left(new InvalidParamError('email'));
+    return isValid ? right(email) : left(new InvalidEmailError(email));
   }
 
-  public isValidName(name: string): Either<InvalidParamError, string> {
+  public isValidName(name: string): Either<InvalidNameError, string> {
     const isValid = isValidName(name);
-    return isValid ? right(name) : left(new InvalidParamError('name'));
+    return isValid ? right(name) : left(new InvalidNameError(name));
   }
 
-  public isValidPassword(password: string): Either<InvalidParamError, string> {
+  public isValidPassword(password: string): Either<InvalidPasswordError, string> {
     const isValid = isValidPassword(password);
-    return isValid ? right(password) : left(new InvalidParamError('password'));
+    return isValid ? right(password) : left(new InvalidPasswordError(password));
   }
 
-  public isValidBio(bio: string): Either<InvalidParamError, string> {
+  public isValidBio(bio: string): Either<InvalidBioError, string> {
     const isValid = isValidBio(bio);
-    return isValid ? right(bio) : left(new InvalidParamError('bio'));
+    return isValid ? right(bio) : left(new InvalidBioError(bio));
   }
 
-  public build(data: user): Either<InvalidParamError, userStoreDTO> {
+  public build(data: user): buildUserContracts {
     const Objects: ObjectsBuildType = {
       name: this.isValidName(data.name),
       email: this.isValidEmail(data.email),
