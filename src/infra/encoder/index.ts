f48fce4361a3d8f8bd382c02a16a64f7a51f5b8e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-11 10:28:36 +0200

    feat: createimplementation of  encoder

---
+++ src/infra/encoder/index.ts
@@ -0,0 +1,15 @@
+import { IEncoder } from '@/data/contracts/encoder';
+
+import bcrypt from 'bcryptjs';
+
+export class Encoder implements IEncoder {
+  async encode(value: string, salt = '10d'): Promise<string> {
+    const hash = await bcrypt.hash(value, salt);
+    return hash;
+  }
+
+  async decode(value: string, valueHash: string): Promise<boolean> {
+    const hash = await bcrypt.compare(value, valueHash);
+    return hash;
+  }
+}
