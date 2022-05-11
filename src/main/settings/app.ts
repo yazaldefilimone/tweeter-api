9ae85037e905c1d179705e77e05f4c2856791f7c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-11 22:36:31 +0200

    feat: add routes of app

--- src/main/settings/app.ts
+++ src/main/settings/app.ts
@@ -1,11 +1,14 @@
+import { routes } from '@/main/routes';
+
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
-
 const app = express();
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 export { app };
