3fe3693d559d9b0cd7b97a0ac9ecf7264390a493
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-11 09:51:45 +0200

    feat: add sigin and login methods of interface userUsecase

--- src/domain/user/use-cases/IUserUseCase.ts
+++ src/domain/user/use-cases/IUserUseCase.ts
@@ -1,14 +1,14 @@
-import { user, userParam, userResponse } from '@/domain/user/dtos';
-import { loginUserContracts, signUserContracts } from './contracts';
+import { user, userParam } from '../dtos';
+import { findUserContracts, loginUserContracts, signUserContracts } from './contracts';
 
 export interface IUserUseCase {
-  signup: (data: IUserUseCase.signOutput) => IUserUseCase.signOutput;
+  signup: (data: IUserUseCase.signInput) => IUserUseCase.signOutput;
   login: (data: IUserUseCase.loginInput) => IUserUseCase.loginOutput;
 }
-
 export namespace IUserUseCase {
   export type signInput = user;
   export type signOutput = Promise<signUserContracts>;
   export type loginInput = userParam;
   export type loginOutput = Promise<loginUserContracts>;
+  export type findOutput<T> = Promise<findUserContracts<T>>;
 }
