4163ee6cd094a58aa51a02fb32d91dc41c12176c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-11 10:13:25 +0200

    feat: change return of findByEmail

--- src/data/repositories/user/IUserRepository.ts
+++ src/data/repositories/user/IUserRepository.ts
@@ -1,10 +1,10 @@
-import { user, userParam, userResponse } from '@/domain/user/dtos';
+import { user, userParam, userResponse, userStoreDTO } from '@/domain/user/dtos';
 
 export interface IUserRepository {
   add: (data: IUserRepository.addInput) => IUserRepository.addOutput;
   findById: ({ id }: { id: string }) => IUserRepository.addOutput;
   findByName: ({ name }: { name: string }) => IUserRepository.findOutput<userResponse[]>;
-  findByEmail: ({ email }: { email: string }) => IUserRepository.findOutput<userResponse>;
+  findByEmail: ({ email }: { email: string }) => IUserRepository.findOutput<userStoreDTO>;
   delete: ({ id }: { id: string }) => Promise<{ id: string }>;
   update: (data: IUserRepository.addInput) => IUserRepository.addOutput;
 }
