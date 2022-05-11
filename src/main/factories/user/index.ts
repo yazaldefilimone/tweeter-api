b4e556c5a5ae788106538c8c06e324835a2a2d4d
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-11 22:51:26 +0200

    feat: create makeLoginFacture

--- src/main/factories/user/index.ts
+++ src/main/factories/user/index.ts
@@ -1,7 +1,7 @@
 import { UserUseCase } from '@/data/use-cases/user';
 import { Encoder } from '@/infra/encoder';
 import { UserRepository } from '@/infra/prisma/repositories/user';
-import { SignUpUserController } from '@/presentation/controllers/user/SignUpUserController';
+import { SignUpUserController, LoginUserController } from '@/presentation/controllers/user';
 import { Request, Response } from 'express';
 
 const userRepository = new UserRepository();
@@ -11,5 +11,11 @@ const userUseCase = new UserUseCase(userRepository, encoder);
 export const makeSignUpFacture = async function (request: Request, response: Response) {
   const signUpUserController = new SignUpUserController(userUseCase);
   const controller = await signUpUserController.execute(request, response);
+  return controller;
+};
+
+export const makeLoginFacture = async function (request: Request, response: Response) {
+  const loginUserController = new LoginUserController(userUseCase);
+  const controller = await loginUserController.execute(request, response);
   return controller;
 };
