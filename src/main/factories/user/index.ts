609fed30609356d04e7bcd2035030d00572547a7
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-16 23:04:54 +0200

    feat: create facute of update user

--- src/main/factories/user/index.ts
+++ src/main/factories/user/index.ts
@@ -3,6 +3,7 @@ import { Encoder } from '@/infra/encoder';
 import { UserRepository } from '@/infra/prisma/repositories/user';
 import { CacheServices } from '@/infra/services/cache';
 import { SignUpUserController, LoginUserController } from '@/presentation/controllers/user';
+import { UpdateUserProfileController } from '@/presentation/controllers/user/UpdateUserController';
 import { Request, Response } from 'express';
 
 const userRepository = new UserRepository();
@@ -18,6 +19,12 @@ export const makeSignUpFacture = async function (request: Request, response: Res
 
 export const makeLoginFacture = async function (request: Request, response: Response) {
   const loginUserController = new LoginUserController(userUseCase);
+  const controller = await loginUserController.execute(request, response);
+  return controller;
+};
+
+export const makeUpdateProfileFacture = async function (request: Request, response: Response) {
+  const loginUserController = new UpdateUserProfileController(userUseCase);
   const controller = await loginUserController.execute(request, response);
   return controller;
 };
