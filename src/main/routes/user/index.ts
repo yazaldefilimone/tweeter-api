d844aea9527b6e604e9aa15e79720c29509a703a
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-16 23:06:05 +0200

    feat: register router of update user

--- src/main/routes/user/index.ts
+++ src/main/routes/user/index.ts
@@ -1,9 +1,10 @@
-import { makeSignUpFacture, makeLoginFacture } from '@/main/factories/user';
+import { makeSignUpFacture, makeLoginFacture, makeUpdateProfileFacture } from '@/main/factories/user';
+import { authUserJwtMiddleware } from '@/main/middlewares/auth-middlewares';
 import { Router } from 'express';
 
 const usersRouter = Router();
 
 usersRouter.post('/signup', makeSignUpFacture);
 usersRouter.post('/login', makeLoginFacture);
-
+usersRouter.put('/update', authUserJwtMiddleware, makeUpdateProfileFacture);
 export { usersRouter };
