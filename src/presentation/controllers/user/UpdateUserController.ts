7b4efb2d204229ce25fc23125c116c4c78427ccb
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-16 23:05:46 +0200

    feat create UpdateUserProfileController

---
+++ src/presentation/controllers/user/UpdateUserController.ts
@@ -0,0 +1,23 @@
+import { IUserUseCase } from '@/domain/user/use-cases';
+import { Request, Response } from 'express';
+
+export class UpdateUserProfileController {
+  private readonly userUseCase: IUserUseCase;
+  constructor(userUseCase: IUserUseCase) {
+    this.userUseCase = userUseCase;
+  }
+  async execute(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, bio, website_url, localization, birth_date } = request.body;
+
+    const userOrError = await this.userUseCase.updateProfile({
+      id: request.userId,
+      data: { name, email, password, website_url, bio, localization, birth_date },
+    });
+
+    if (userOrError.isLeft()) {
+      return response.status(401).json({ message: userOrError.value.message });
+    }
+
+    return response.status(201).json(userOrError.value);
+  }
+}
