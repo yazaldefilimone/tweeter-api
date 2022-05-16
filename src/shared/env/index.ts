6f55e37002da1d67ff2a65a352b26019f6bb0b57
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-16 18:36:13 +0200

    fix: add crepto of md5

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -1,7 +1,7 @@
 export const env = {
   port: process.env.PORT || '3003',
   security: {
-    key: process.env.HASH || 'Haxuuywecusvcuidc2edxvcwdue',
+    key: process.env.HASH || '1614a81db056654f4d9a6d9aa3643ae8',
   },
   s3: {},
   redis: {
