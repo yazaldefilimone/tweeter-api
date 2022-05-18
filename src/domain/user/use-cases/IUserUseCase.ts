8eb3056a67948c51ce5b9ef55bbae50c92234e24
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-18 18:25:23 +0200

    feat: create method of update avatr and update banner

--- src/domain/user/use-cases/IUserUseCase.ts
+++ src/domain/user/use-cases/IUserUseCase.ts
@@ -5,10 +5,13 @@ export interface IUserUseCase {
   signup: (data: IUserUseCase.signInput) => IUserUseCase.signOutput;
   login: (data: IUserUseCase.loginInput) => IUserUseCase.loginOutput;
   updateProfile: (data: IUserUseCase.updateInput) => IUserUseCase.signOutput;
+  updateAvatar: (data: { avatar: string }) => IUserUseCase.updateOutput;
+  updateBanner: (data: { banner: string }) => IUserUseCase.updateOutput;
 }
 export namespace IUserUseCase {
   export type signInput = user;
   export type updateInput = userUpdateDTO;
+  export type updateOutput = Promise<{ id: string }>;
   export type signOutput = Promise<signUserContracts>;
   export type loginInput = userParam;
   export type loginOutput = Promise<loginUserContracts>;
