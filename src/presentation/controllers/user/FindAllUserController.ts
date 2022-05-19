e52fb4d447d810b9a032ac83447368ac1caa61c7
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-20 00:14:06 +0200

    feat: add find all user

---
+++ src/presentation/controllers/user/FindAllUserController.ts
@@ -0,0 +1,21 @@
+import { IUserUseCase } from '@/domain/user/use-cases';
+import { Request, Response } from 'express';
+
+export class FindAllUserController {
+  private readonly userUseCase: IUserUseCase;
+  constructor(userUseCase: IUserUseCase) {
+    this.userUseCase = userUseCase;
+  }
+
+  async execute(request: Request, response: Response): Promise<Response> {
+    const usersOrError = await this.userUseCase.findAll();
+
+    if (usersOrError.isLeft()) {
+      return response.status(400).json({ message: usersOrError.value.message });
+    }
+
+    return response.status(201).json({
+      ...usersOrError.value,
+    });
+  }
+}
