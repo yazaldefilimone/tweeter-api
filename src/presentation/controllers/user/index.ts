240144630d586a6f30041d1d36bc0ea0d570adc9
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-19 23:15:57 +0200

    style: add UpdateAvatarController and UpdateBannerController and export method

--- src/presentation/controllers/user/index.ts
+++ src/presentation/controllers/user/index.ts
@@ -1,2 +1,5 @@
 export * from './LoginUserController';
 export * from './SignUpUserController';
+export * from './UpdateUserController';
+export * from './UpdateAvatarController';
+export * from './UpdateBannerController';
