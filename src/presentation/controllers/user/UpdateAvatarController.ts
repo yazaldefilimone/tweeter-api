6584ee536fdb0eedd7a70b1ff4c291b86c3d7d37
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-19 23:15:28 +0200

    feat: UpdateAvatar Controller

---
+++ src/presentation/controllers/user/UpdateAvatarController.ts
@@ -0,0 +1,33 @@
+import { IUserUseCase } from '@/domain/user/use-cases';
+import { Request, Response } from 'express';
+
+export class UpdateAvatarController {
+  private readonly userUseCase: IUserUseCase;
+  constructor(userUseCase: IUserUseCase) {
+    this.userUseCase = userUseCase;
+  }
+
+  async execute(request: Request, response: Response): Promise<Response> {
+    if (!request.file) {
+      return response.status(204).json({ message: 'file is required' });
+    }
+
+    const fileOrUndefined = request.file;
+    const userId = request.userId;
+
+    if (!fileOrUndefined) {
+      return response.status(400).send({ message: 'Image file is required' });
+    }
+
+    const fileSavedName = fileOrUndefined.filename;
+
+    const userOrError = await this.userUseCase.updateAvatar({ avatar: fileSavedName, id: userId });
+
+    if (userOrError.isLeft()) {
+      return response.status(401).json({ message: userOrError.value.message });
+    }
+    return response.status(200).send({
+      avatar_url: fileSavedName,
+    });
+  }
+}
