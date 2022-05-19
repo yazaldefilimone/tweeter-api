ba5370a8c25851d3d92aee2e7ed4d6acdf1134a9
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-20 00:33:47 +0200

    style: add find method of export filr

--- src/presentation/controllers/user/index.ts
+++ src/presentation/controllers/user/index.ts
@@ -3,3 +3,6 @@ export * from './SignUpUserController';
 export * from './UpdateUserController';
 export * from './UpdateAvatarController';
 export * from './UpdateBannerController';
+export * from './FindAllUserController';
+export * from './FindByNameUserController';
+export * from './FindByIdUserController';
