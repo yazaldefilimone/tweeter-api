bb756d5548f8e46894fbbd73fe6d81ca61cdb65d
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-21 14:01:54 +0200

    feat: create method of create key f cache and method findByid

--- src/data/use-cases/post/PostUseCase.ts
+++ src/data/use-cases/post/PostUseCase.ts
@@ -1,5 +1,7 @@
 import { IPostRepository } from '@/data/repositories/post';
 import { ICacheServices } from '@/data/services/cache/ICacheServices';
+import { NotFoundError } from '@/domain/errors';
+import { postStorage } from '@/domain/post/dtos';
 import { Post } from '@/domain/post/entity/post';
 import { IPostUseCase } from '@/domain/post/use-cases';
 import { left, right } from '@/shared/error-handler/either';
@@ -12,6 +14,10 @@ export class PostUseCase implements IPostUseCase {
     this.cacheServices = cacheServices;
   }
 
+  private id(id: string): string {
+    return `post-${id}`;
+  }
+
   async create(data: IPostUseCase.Input): IPostUseCase.Output {
     const build = new Post().build(data);
     if (build.isLeft()) {
@@ -20,7 +26,19 @@ export class PostUseCase implements IPostUseCase {
     const post = build.value;
 
     const postStore = await this.postRepository.add(post);
-    this.cacheServices.setCache({ key: `post-${postStore.id}`, data: data });
+    this.cacheServices.setCache({ key: this.id(postStore.id), data: data });
     return right(postStore);
+  }
+  async findById({ id }: IPostUseCase.InputId): IPostUseCase.Output {
+    const cache = await this.cacheServices.getCache<postStorage>(this.id(id));
+    if (cache) {
+      return right(cache);
+    }
+    const post = await this.postRepository.findById({ id });
+    if (!post) {
+      return left(new NotFoundError('post'));
+    }
+
+    return right(post);
   }
 }
