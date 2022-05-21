1362d24097d5b5e32bef9d462d9d73ed46b7801b
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-21 13:17:22 +0200

    feat: create  contracts of post

---
+++ src/domain/post/use-cases/contracts/index.ts
@@ -0,0 +1,12 @@
+import { NotFoundError } from '@/domain/errors';
+import { Either } from '@/shared/error-handler/either';
+import { postStorage } from '@/domain/post/dtos';
+import { InvalidContentError } from '@/domain/post/errors';
+
+export type postUseCaseSuccess = { id: string };
+export type postUseCaseSuccessMany = postStorage;
+export type postUseCaseFailed = InvalidContentError | NotFoundError;
+export type postUseCaseFailedMany = NotFoundError;
+
+export type postUseCaseResponse = Either<postUseCaseFailed, postUseCaseSuccess>;
+export type postUseCaseResponseMany = Either<postUseCaseFailedMany, postUseCaseSuccessMany[]>;
