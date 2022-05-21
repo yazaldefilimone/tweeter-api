99cfef182b78382a9cb64523cd3a5cd69be0eb73
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-21 13:50:29 +0200

    feat: remove types

--- src/domain/post/use-cases/contracts/index.ts
+++ src/domain/post/use-cases/contracts/index.ts
@@ -3,10 +3,10 @@ import { Either } from '@/shared/error-handler/either';
 import { postStorage } from '@/domain/post/dtos';
 import { InvalidContentError } from '@/domain/post/errors';
 
-export type postUseCaseSuccess = { id: string };
-export type postUseCaseSuccessMany = postStorage;
+export type postUseCaseSuccess = postStorage;
+export type postUseCaseSuccessMany = postStorage[];
 export type postUseCaseFailed = InvalidContentError | NotFoundError;
 export type postUseCaseFailedMany = NotFoundError;
 
 export type postUseCaseResponse = Either<postUseCaseFailed, postUseCaseSuccess>;
-export type postUseCaseResponseMany = Either<postUseCaseFailedMany, postUseCaseSuccessMany[]>;
+export type postUseCaseResponseMany = Either<postUseCaseFailedMany, postUseCaseSuccessMany>;
