c9d70a37d93aeffe92a16fff3126b9bafadad821
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-21 14:26:55 +0200

    feat: create type of delete output

--- src/domain/post/use-cases/IPostUseCase.ts
+++ src/domain/post/use-cases/IPostUseCase.ts
@@ -1,9 +1,11 @@
+import { NotFoundError } from '@/domain/errors';
+import { Either } from '@/shared/error-handler/either';
 import { post } from '../dtos';
 import { postUseCaseFailedMany, postUseCaseResponse, postUseCaseResponseMany, postUseCaseSuccess } from './contracts';
 
 export interface IPostUseCase {
   create: (data: IPostUseCase.Input) => IPostUseCase.Output;
-  deleteById: (data: { id: string; userId: string }) => IPostUseCase.Output;
+  deleteById: (data: { id: string; userId: string }) => IPostUseCase.DeleteOutput;
   findByUserId: (data: { userId: string; limit: number; page: number }) => IPostUseCase.OutputMany;
   findById: (data: IPostUseCase.InputId) => IPostUseCase.Output;
   find: (data: { limit: number; page: number }) => IPostUseCase.OutputMany;
@@ -14,4 +16,5 @@ export namespace IPostUseCase {
   export type InputId = { id: string };
   export type Output = Promise<postUseCaseResponse>;
   export type OutputMany = Promise<postUseCaseResponseMany>;
+  export type DeleteOutput = Promise<Either<NotFoundError, { id: string }>>;
 }
