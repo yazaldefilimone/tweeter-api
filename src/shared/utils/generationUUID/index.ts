41ccea830cb7e724ef9dffb14fe71bd270934258
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-21 13:49:23 +0200

    fix: change param of object

--- src/shared/utils/generationUUID/index.ts
+++ src/shared/utils/generationUUID/index.ts
@@ -1,6 +1,6 @@
 import { v4 } from 'uuid';
 
-export const generationUUID = function (isDomain = false) {
+export const generationUUID = function ({ isDomain }: { isDomain: boolean }) {
   if (isDomain) {
     return v4();
   }
